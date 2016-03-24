c3b79bd2818258d4ee8dd3381938ad228b7156e9
Marius Gundersen <me@mariusgundersen.net>
2016-03-25 00:32:52 +0100

    added service worker

---
+++ public/service-worker.js
@@ -0,0 +1,82 @@
+var version = '2016-3-28-18:58:55';
+
+self.addEventListener('install', function(event) {
+  console.log('[ServiceWorker] Installed version', version);
+  event.waitUntil(
+    fetch('./mymodule.js').then(function(response) {
+      return caches.open(version).then(function(cache) {
+        console.log('[ServiceWorker] Cached mymodule.js for', version);
+        // Important to `return` the promise here to have `skipWaiting()`
+        // fire after the cache has been updated.
+        return cache.put('mymodule.js', response);
+      });
+    }).then(function() {
+      // `skipWaiting()` forces the waiting ServiceWorker to become the
+      // active ServiceWorker, triggering the `onactivate` event.
+      // Together with `Clients.claim()` this allows a worker to take effect
+      // immediately in the client(s).
+      console.log('[ServiceWorker] Skip waiting on install');
+      return self.skipWaiting();
+    })
+  );
+});
+
+// `onactivate` is usually called after a worker was installed and the page
+// got refreshed. Since we call `skipWaiting()` in `oninstall`, `onactivate` is
+// called immediately.
+self.addEventListener('activate', function(event) {
+  // Just for debugging, list all controlled clients.
+  self.clients.matchAll({
+    includeUncontrolled: true
+  }).then(function(clientList) {
+    var urls = clientList.map(function(client) {
+      return client.url;
+    });
+    console.log('[ServiceWorker] Matching clients:', urls.join(', '));
+  });
+
+  event.waitUntil(
+    // Delete old cache entries that don't match the current version.
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== version) {
+            console.log('[ServiceWorker] Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(function() {
+      // `claim()` sets this worker as the active worker for all clients that
+      // match the workers scope and triggers an `oncontrollerchange` event for
+      // the clients.
+      console.log('[ServiceWorker] Claiming clients for version', version);
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('[ServiceWorker] fetching', event.request.url);
+  if (event.request.url.includes('/mymodule.js')) {
+    console.log('[ServiceWorker] Serving mymodule.js for', event.request.url);
+    event.respondWith(
+      caches.open(version).then(function(cache) {
+        cache.keys().then(keys => console.log('[ServiceWorker] keys:', keys));
+        return cache.match('mymodule.js').then(function(response) {
+          if (!response) {
+            console.error('[ServiceWorker] Missing cache!');
+          }
+          return response;
+        });
+      })
+    );
+  }
+  if (event.request.url.includes('/version')) {
+    event.respondWith(new Response(version, {
+      headers: {
+        'content-type': 'text/plain'
+      }
+    }));
+  }
+});
--- server.js
+++ server.js
@@ -4,13 +4,16 @@ var fs = require('fs');
 var http2 = require('http2');
 var Router = require('router');
 
+express.static.mime.define({'application/javascript': ['js']});
+
 var router = new Router();
-
-router.use('/node_modules',express.static('node_modules'));
+router.use('/node_modules',express.static('node_modules', {
+  maxAge: 0
+}));
 router.use('/',express.static('public'));
-router.get('/', function (req, res) {
+router.get('/test', function (req, res) {
     console.log(req.headers);
-
+    console.log(res.push);
     res.setHeader('Content-type', 'text/html');
     return res.end('<h1>Hello, Secure World!</h1>');
 });
