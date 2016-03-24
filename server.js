91b87c00dd139c66e69cd0cbc2b6e823bb7b8789
Marius Gundersen <me@mariusgundersen.net>
2016-03-24 23:29:17 +0100

    switched to http2

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 var express = require('express');
+var finalhandler = require('finalhandler');
+var fs = require('fs');
+var http2 = require('http2');
+var Router = require('router');
 
-var app = express();
-app.use('/node_modules',express.static('node_modules'));
-app.use(express.static('public'));
-app.listen(8080);
+var router = new Router();
+
+router.use('/node_modules',express.static('node_modules'));
+router.use('/',express.static('public'));
+router.get('/', function (req, res) {
+    console.log(req.headers);
+
+    res.setHeader('Content-type', 'text/html');
+    return res.end('<h1>Hello, Secure World!</h1>');
+});
+var options = {
+  key: fs.readFileSync('./certs/localhost.key'),
+  cert: fs.readFileSync('./certs/localhost.crt')
+};
+
+http2.createServer(options, app).listen(8080);
+function app(req, res) {
+    router(req, res, finalhandler(req, res));
+}
